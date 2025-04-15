334aee1a3da917a4ac8f34699a010fb8f6403077
Max-J Rosalbert <rosalmjo@gmail.com>
2025-04-15 08:28:38 -0400

    ajustement de function.php

--- functions.php
+++ functions.php
@@ -1,19 +1,11 @@
 <?php
+function kb_biz_flick_enqueue_styles() {
+    // Charger le style parent.
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-
-function enqueue_parent_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    // Charger le style enfant avec comme dépendance le style parent.
+    // Ainsi, le style enfant est chargé après et pourra surcharger les règles du parent.
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/assets/sass/styles.css', array( 'parent-style' ), null );
 }
-
-//load child theme custom CSS
-
-function kb_biz_flick_child_enqueue_styles() {
-    // $parenthandle = 'biz-flick-style'; // This is 'style' for the Biz Flick theme.
-    // é$theme = wp_get_theme();
-    wp_enqeue_style( 'styles', get_stylesheet_directory_uri() . '/assets/sass/styles.css');
-    // wp_enqueue_style( 'custom-style', get_stylesheet_uri());
-}
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-add_action( 'wp_enqueue_scripts', 'kb_biz_flick_child_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'kb_biz_flick_enqueue_styles' );
 ?>
